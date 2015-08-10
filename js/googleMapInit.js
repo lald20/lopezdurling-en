7bb226e613986df431ffcb741636544566fb548a
Gabriel Reges <gregesf@bluetideconsulting.com>
2015-08-10 12:19:56 -0500

    subida inicial de website

---
+++ js/googleMapInit.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/* google */
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+function initialize() {
+    var map_canvas = document.getElementById('googleMap');
+
+    var map_options = {
+        center: new google.maps.LatLng(8.981396, -79.524764),
+        zoom: 17,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false,
+        streetViewControl: false
+    };
+
+    var map = new google.maps.Map(map_canvas, map_options);
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(8.981396, -79.524764),
+        map: map,
+        title: 'López | Durling'
+    });
+    var styles = [{"stylers":[{"hue":"#ff1a00"},{"invert_lightness":true},{"saturation":-100},{"lightness":33},{"gamma":0.5}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2D333C"}]}]
+    map.setOptions({styles: styles});
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+});
