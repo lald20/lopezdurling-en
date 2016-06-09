254741afb0c0daf8611fe6e06e22b1a83a468b6b
Gabriel Reges <gregesf@bluetideconsulting.com>
2016-06-09 17:52:19 -0500

    variable global para listado de clientes

--- lopezdurling/plugins/page_context.py
+++ lopezdurling/plugins/page_context.py
@@ -1,4 +1,27 @@
 #coding:utf-8
+def client_list():
+    return [
+        {
+            "IMAGE": "../static/img/clients/1.png",
+            "NAME": "BB&M Publicidad",
+            "URL": "#",
+        },{
+            "IMAGE": "../static/img/clients/2.png",
+            "NAME": "MetroBank",
+            "URL": "#",
+        }
+        ,{
+            "IMAGE": "../static/img/clients/3.png",
+            "NAME": "Óptica López",
+            "URL": "#",
+        }
+        ,{
+            "IMAGE": "../static/img/clients/4.png",
+            "NAME": "Óptica López",
+            "URL": "#",
+        }
+        
+    ]
 
 def preBuildPage(page, context, data):
     """
@@ -9,7 +32,8 @@ def preBuildPage(page, context, data):
     # Any changes you make to context will be passed to the template renderer for this page.
 
     extra = {
-        "CURRENT_PAGE": page
+        "CURRENT_PAGE": page,
+        "CLIENT_LIST": client_list(), 
         # Add your own dynamic context elements here!
     }
 
