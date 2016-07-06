16f9e9c9bb4ba6cce363a3957fb57579806bd567
Gabriel Reges <gregesf@bluetideconsulting.com>
2016-07-06 16:52:44 -0500

    cambios en datos de contacto y dirección

--- lopezdurling/static/js/googleMapInit.js
+++ lopezdurling/static/js/googleMapInit.js
@@ -7,7 +7,7 @@ function initialize() {
     var map_canvas = document.getElementById('googleMap');
 
     var map_options = {
-        center: new google.maps.LatLng(8.981396, -79.524764),
+        center: new google.maps.LatLng(8.994291, -79.502781),
         zoom: 17,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         scrollwheel: false,
@@ -16,7 +16,7 @@ function initialize() {
 
     var map = new google.maps.Map(map_canvas, map_options);
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(8.981396, -79.524764),
+        position: new google.maps.LatLng(8.994291, -79.502781),
         map: map,
         icon: "../static/img/pin.png",
         title: 'López | Durling'
